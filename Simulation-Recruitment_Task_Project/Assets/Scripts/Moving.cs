3620151b4c97e5629beab5758efa5dca978545cb
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-12 14:35:41 +0200

    ProcessGuideScript and Moving scripts have been improved

--- Simulation-Recruitment_Task_Project/Assets/Scripts/Moving.cs
+++ Simulation-Recruitment_Task_Project/Assets/Scripts/Moving.cs
@@ -21,7 +21,7 @@ public class Moving : MonoBehaviour
         
         if (CurrentPosition.x >= NewPosition.x - 0.2f && CurrentPosition.x <= NewPosition.x + 0.2f && CurrentPosition.z >= NewPosition.z - 0.2f && CurrentPosition.z <= NewPosition.z + 0.2f)
         {
-            NewPosition = new Vector3(Random.Range(-10.0f, 10.0f), 1.0f, Random.Range(-10.0f, 10.0f));
+            NewPosition = new Vector3(Random.Range(-9.0f, 9.0f), 1.0f, Random.Range(-9.0f, 9.0f));
             NavigationAgent.SetDestination(NewPosition);
         }
         CurrentPosition = transform.position;
--- Simulation-Recruitment_Task_Project/Assets/Scripts/ProcessGuideScript.cs
+++ Simulation-Recruitment_Task_Project/Assets/Scripts/ProcessGuideScript.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class ProcessGuideScript : MonoBehaviour
 {
     public List<GameObject> AgentsList;
-    public float TimeToSpawn = 5.0f;
+    public float TimeToSpawn;
     public GameObject AgentTemplate;
     float time;
 
     // Start is called before the first frame update
     void Start()
     {
-        time = TimeToSpawn;
+        if (TimeToSpawn > 2 && TimeToSpawn < 10)
+        {
+            time = TimeToSpawn;
+        }
+        else
+        {
+            if (TimeToSpawn < 2)
+            {
+                TimeToSpawn = 2;
+            }
+            else if (TimeToSpawn > 10)
+            {
+                TimeToSpawn = 10;
+            }
+        }
         AgentsList.Add(Instantiate(AgentTemplate, transform));
         time -= Time.deltaTime;
     }
@@ -20,6 +34,32 @@ public class ProcessGuideScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (TimeToSpawn > 2 && TimeToSpawn < 10)
+        {
+            time = TimeToSpawn;
+        }
+        else
+        {
+            if (TimeToSpawn < 2)
+            {
+                TimeToSpawn = 2;
+            }
+            else if (TimeToSpawn > 10)
+            {
+                TimeToSpawn = 10;
+            }
+        }
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        if (time <= 0)
+        {
+            time = TimeToSpawn;
+            if (AgentsList.Count < 30)
+            {
+                AgentsList.Add(Instantiate(AgentTemplate, transform));
+            }
+        }
     }
 }
