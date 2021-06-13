a921a814a42efb9bd7e6a41b3dc2e05443ec8207
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-13 20:16:19 +0200

    Improved MarcoPoloNumbers

--- Simulation-Recruitment_Task_Project/Assets/Scripts/MarcoPoloNumbers.cs
+++ Simulation-Recruitment_Task_Project/Assets/Scripts/MarcoPoloNumbers.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class MarcoPoloNumbers : MonoBehaviour
 {
+    
     public void NumbersAndMarcoDisplay()
     {
         for (int i = 1;i<= 100; i++)
         {
             Debug.Log(i);
+            
+            if (i % 3 == 0 && i % 5 != 0)
+            {
+                Debug.Log("Marco");
+            }
+            if (i % 3 != 0 && i % 5 == 0)
+            {
+                Debug.Log("Polo");
+            }
             
         }
     }
