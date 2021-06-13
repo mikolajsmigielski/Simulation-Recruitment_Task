827340c70b7f7d63b7176e1d8f042854274eb6a3
mikolajsmigielski <mikolaj.smigielski@gmail.com>
2021-06-13 19:14:17 +0200

    MarcoPoloNumbers was created

---
+++ Simulation-Recruitment_Task_Project/Assets/Scripts/MarcoPoloNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarcoPoloNumbers : MonoBehaviour
+{
+    public void NumbersAndMarcoDisplay()
+    {
+        for (int i = 1;i<= 100; i++)
+        {
+            Debug.Log(i);
+            
+        }
+    }
+}
